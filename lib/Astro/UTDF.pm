66a930fa5a6b9142a6910eba9461ef3d203655bb
Tom Wyant <wyant@cpan.org>
2014-10-19 15:40:36 +0000

    Import of WYANT/Astro-UTDF-0.004_01 from CPAN.
    gitpan-cpan-distribution: Astro-UTDF
    gitpan-cpan-version:      0.004_01
    gitpan-cpan-path:         WYANT/Astro-UTDF-0.004_01.tar.gz
    gitpan-cpan-author:       WYANT
    gitpan-cpan-maturity:     developer

--- lib/Astro/UTDF.pm
+++ lib/Astro/UTDF.pm
@@ -14,7 +14,7 @@ use constant PI => atan2( 0, -1 );
 use constant TWO_PI => 2 * PI;
 use constant SPEED_OF_LIGHT => 299792.458;	# Km/sec, per U.S. NIST
 
-our $VERSION = '0.004';
+our $VERSION = '0.004_01';
 
 sub new {
     my $class = shift;
@@ -1769,7 +1769,7 @@ Thomas R. Wyant, III F<wyant at cpan dot org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2010-2013 Thomas R. Wyant, III
+Copyright (C) 2010-2014 Thomas R. Wyant, III
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl 5.10.0. For more details, see the full text
